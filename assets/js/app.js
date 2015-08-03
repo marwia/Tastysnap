06c584f1fed5ddd2817720ebd18f476efcd652ef
marwia <mariusz.wiazowski@gmail.com>
2015-08-03 11:31:17 +0200

    Aggiunti commenti che tentano di spiegare il funzionamento dell'applicazione Angular

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,8 @@
  * Il modulo "angular-loading-bar" serve a mostrare la barra di progresso ogni
  * volta che viene fatta una richiesta HTTP.
  *
+ * In questa parte di codice dichiaro tutte le dipendenze relative al modulo principale chiamato "sampleApp". 
+ *
  */
 
 angular.module('sampleApp', ['ui.router', 'angular-loading-bar', 'ui.bootstrap', 'ngCookies', 'ngAnimate', 'appRoutes', 'AuthCtrl', 'MainCtrl', 'NavCtrl', 'PostsCtrl', 'AuthService', 'PostService', 'MasterCtrl']);
