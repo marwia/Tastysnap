e04e78a9da25c21b48cb9904abf27713e17cca32
Mariusz <mariusz.wiazowski@elitetech.it>
2016-03-04 21:32:26 +0100

    Creato modello per i prodotti sulla base del USDA; Creato il controller per i prodotti;

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -5,31 +5,73 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+
+/**
+ * Module dependencies
+ * 
+ * Prese da https://github.com/balderdashy/sails/blob/master/lib/hooks/blueprints/actions/find.js
+ */
+var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
+
 module.exports = {
 	
 	/**
-     * @api {get} /product/categories Get product categories
-     * @apiName GetProductCategories
+     * @api {get} /product/groups Get product groups
+     * @apiName GetProductGroups
      * @apiGroup Product
      *
      * @apiDescription Serve per ottenere la lista dei vari tipi di 
-     * categorie di prodotti.
+     * gruppi di prodotti.
      * 
-     * @apiSuccess {json} recipe JSON that represents the dosage types object.
+     * @apiSuccess {json} group_def JSON that represents the recipe groups object.
      *
      * @apiSuccessExample {json} Success-Response-Example:
      *     HTTP/1.1 200 OK
      *     {
      *          "type": "string",
      *          "enum": [
-     *               "breads",
-     *               "condiments"
+     *               "...",
+     *               "..."
      *          ]
      *      }
      *
      */
-    getProductCategories: function (req, res) {
-        return res.json(sails.models.product.definition.category);
+    getProductGroups: function (req, res) {
+        return res.json(sails.models.product.definition.group);
+    },
+    
+    /**
+     * @api {get} /product Get product list
+     * @apiName GetProductList
+     * @apiGroup Product
+     *
+     * @apiDescription Serve per ottenere la lista di prodotti.
+     * Attenzione che i risultati sono limitati ad un numero preciso di prodotti, massimo 30 per richiesta.<br>
+     * Questo end point accetta prametri.
+     *
+     * @apiParam {Integer} skip The number of records to skip (useful for pagination).
+     * @apiParam {Integer} limit The maximum number of records to send back (useful for pagination). Defaults to 30. 
+     * @apiParam {String} where Instead of filtering based on a specific attribute, you may instead choose to provide a where parameter with a Waterline WHERE criteria object, encoded as a JSON string.
+     * @apiParam {String} sort The sort order. By default, returned records are sorted by primary key value in ascending order. 
+     *
+     * @apiParamExample Request-Param-Example:
+     *     ?skip=6&limit=3
+     *
+     * 
+     * @apiSuccess {array} product_list Array of JSON that represents the product objects.
+     *
+     */
+    find: function (req, res, next) {
+        Product.find()
+            .where(actionUtil.parseCriteria(req))
+            .limit(actionUtil.parseLimit(req))
+            .skip(actionUtil.parseSkip(req))
+            .sort(actionUtil.parseSort(req))
+            .exec(function (err, foundRecipes) {
+                if (err) { return next(err); }
+            
+                return res.json(foundRecipes);
+            });
     }
 	
 };
--- api/models/Product.js
+++ api/models/Product.js
@@ -13,25 +13,37 @@ module.exports = {
 
   attributes: {
 
-  	name : { type: 'String', required: true },
-
-  	category: {
-    	type: 'String',
-    	required: true, 
-    	enum: ['breads', 
-    	'condiments', 
-    	'butter', 'cheese', 'milk', 'yogurts',
-    	'eggs', 
-    	'fruits', 'vegetables', 
-    	'meat', 
-    	'seafood', 
-    	'sauce', 
-    	'pasta', 'rice', 'legumes',
-    	'flour',
-    	'edible fungi',
-    	'nuts and seeds',
-    	'cereals']
-  	},
+  	name : { type: 'json' },
+      
+    group : { 
+        type: 'String', 
+        enum: ['Caseari e ovoprodotti', 
+    	'Spezie ed erbe', 
+    	'Alimenti per bambini', 
+        'Grassi e oli', 
+        'Zuppe, salse e sughi', 
+        'Cereali da colazine',
+    	'Frutta e succhi di frutta', 
+    	'Maiale', 'Verdure e derivati', 
+    	'Lavorati di noci e semi', 
+    	'Manzo e derivati', 
+    	'Bevande', 
+    	'Pesce e frutti di mare', 
+        'Legumi e derivati', 
+        'Agnello, vitello e carni di selvaggina',
+    	'Prodotti da forno',
+    	'Dolci',
+    	'Cereali e pasta',
+    	'Fast Food',
+        'Antipasti e contorni',
+        'Snacks',
+        'Cibi di nativi americani',
+        'Cibi da ristoranti',]
+    },
+    
+    portions : { type: 'Array' },
+      
+    nutrients : { type: 'Array' },
 
   	// Reference to many Ingredients
   	// Because a product can become an ingredient many times
@@ -40,6 +52,8 @@ module.exports = {
       via: 'product'
     }
 
-  }
+  },
+  
+  connection: 'someMongodbServer'
 };
 
