3928a45d66b1408a9e4271896b578c46c008ef35
Mariusz <mariusz.wiazowski@elitetech.it>
2016-03-31 16:36:07 +0200

    Bug fix....

--- config/local.js
+++ config/local.js
@@ -3,20 +3,9 @@ var fs = require('fs');
 /**
  * Configurazione dei vari certificati SSL
  */
-<<<<<<< HEAD
 
-/*
-module.exports = {
-
-    ssl : {
-         ca: fs.readFileSync('/etc/letsencrypt/live/tastysnap.com/chain.pem'),
-        key: fs.readFileSync('/etc/letsencrypt/live/tastysnap.com/privkey.pem'),
-       cert: fs.readFileSync('/etc/letsencrypt/live/tastysnap.com/cert.pem')
-    },
-=======
 if (process.env.NODE_ENV === 'production') {
->>>>>>> 4d0b2e27ce6ab2e978a36fd3582ebddc580bd391
-    
+
     module.exports = {
 
         ssl: {
@@ -27,10 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 
         port: process.env.PORT || 443
 
-<<<<<<< HEAD
-};
-*/
-=======
     };
+
 }
->>>>>>> 4d0b2e27ce6ab2e978a36fd3582ebddc580bd391
+
