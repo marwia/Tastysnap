9441ac102e268f078bcd07150b9cfd8cf6c9dae7
Matteo Bolognesi <matt.bolo92@gmail.com>
2016-04-10 14:11:33 +0200

    fatta prima versione per la ricerca avanzata, è solo html devo implementare con l'aiuto di mario angular per la ricerca, per provare selezionare ricette dal menu a discesa

--- assets/js/controllers/SearchCtrl.js
+++ assets/js/controllers/SearchCtrl.js
@@ -35,6 +35,46 @@ angular.module('SearchCtrl', []).controller('SearchCtrl', [
             User.search($scope.q, skip, successCB, errorCB);
         }
         
+        //variabile per il funzionamento della navbar della ricerca avanzata
+        $scope.isCollapsed = false;
+        
+        //dati per riempire la select "ricerca in" nella navbar della ricerca avanzata - "ricerca in"
+        $scope.items = [
+            { id: 1, name: 'tutto' },
+            { id: 2, name: 'ricette' },
+            { id: 3, name: 'raccolte' },
+            { id: 4, name: 'persone' }
+        ];
+        
+        //dati per riempire la select "categoria" nella navbar della ricerca avanzata - "categoria"
+        $scope.items2 = [
+            { id: 1, name: 'primi piatti' },
+            { id: 2, name: 'secondi piatti' },
+            { id: 3, name: 'instalate' },
+            { id: 4, name: '...' }
+        ];
+        
+        //dati per riempire la select "difficoltà" nella navbar della ricerca avanzata - "difficoltà"
+        $scope.items3 = [
+            { id: 1, name: 'facile' },
+            { id: 2, name: 'media' },
+            { id: 3, name: 'difficile' }
+        ];
+        
+        //dati per riempire la select "costo" nella navbar della ricerca avanzata - "costo"
+        $scope.items4 = [
+            { id: 1, name: 'basso' },
+            { id: 2, name: 'medio' },
+            { id: 3, name: 'alto' }
+        ];
+        
+        //dati per riempire la select "calorie" nella navbar della ricerca avanzata - "calorie"
+        $scope.items5 = [
+            { id: 1, name: 'poco calorica' },
+            { id: 2, name: 'medio calorica' },
+            { id: 3, name: 'molto calorica' }
+        ];
+        
         var init = function () {
             // inizializzazione del controller
             // query per ricette
