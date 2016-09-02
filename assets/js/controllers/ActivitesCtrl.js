9a0880d4b4a3ab8d771f4eae7e5396d35b783621
Matteo Bolognesi <matt.bolo92@gmail.com>
2016-09-02 12:42:48 +0200

    Stile della pagina ATTIVITA + singole attività

--- assets/js/controllers/ActivitesCtrl.js
+++ assets/js/controllers/ActivitesCtrl.js
@@ -9,4 +9,13 @@ angular.module('ActivitesCtrl', []).controller('ActivitesCtrl', [
     '$scope',
     function ($scope) {
 
+        // piccolo script per generare un numero di attività casuali, (esempio da eliminare)
+        $scope.range = function(min, max, step){
+            step = step || 1;
+            var input = [];
+            for (var i = min; i <= max; i += step) input.push(i);
+            return input;
+        };
+
+
     }]);
