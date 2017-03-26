a31713dcd13cbc9ccde102285ee26c69d99c1995
Mariusz Wiazowski <mariusz.wiazowski@gmail.com>
2017-03-26 14:45:01 +0200

    Implementata la possibilità di seguire un utente direttamente dalla card; Corrette le gestioni delle azioni di seguire/semtti di seguire;

--- assets/js/directives/user_card.js
+++ assets/js/directives/user_card.js
@@ -20,12 +20,28 @@ angular.module('sampleApp')
             },
             
             link: function (scope, element, attrs) {
+                scope.currentUser = User.currentUser;
                 scope.getUserProfileImage = User.getUserProfileImage;
-                scope.toggleFollow = User.toggleFollow;
+                
+                // devo passare l'oggeto user in una funzione per problemi di scope sulle ng-repeat
+                scope.onClick = function() {
+                    //devo invertirlo per motivi tecnici legati al uib-btn-checkbox
+                    scope.user.isFollowed = !scope.user.isFollowed;
+                    User.toggleFollow(scope.user);
+                };
+
+                scope.isFollowed = function() {
+                    return scope.user.isFollowed;
+                }
+
                 /**
                  * Inizializzazione del controller
                  */
-                User.areYouFollowing(scope.user);
+                User.areYouFollowing(scope.user, function() {
+                    scope.user.isFollowed = true;
+                }, function() {
+                    scope.user.isFollowed = false;
+                });
             },
             templateUrl: 'templates/user_card.html'
         };
--- assets/js/services/UserService.js
+++ assets/js/services/UserService.js
@@ -7,12 +7,12 @@
  */
 
 angular.module('UserService', [])
-    .factory('User', ['$http', 'Auth', function($http, Auth) {
+    .factory('User', ['$http', 'Auth', function ($http, Auth) {
 
         var server_prefix = '/api/v1';
 
         // service body
-        var service = {
+        var o = {
             currentUser: {},
             user: {},
             following_users: [],//quelli che si segue
@@ -23,9 +23,9 @@ angular.module('UserService', [])
         /**
          * Metodo per richiedere l'utente correntemente loggato.
          */
-        service.getCurrentUser = function(successCB, errorCB) {
-            return $http.get(server_prefix + '/user/' + Auth.currentUser().id).then(function(response) {
-                angular.copy(response.data, service.currentUser);
+        o.getCurrentUser = function (successCB, errorCB) {
+            return $http.get(server_prefix + '/user/' + Auth.currentUser().id).then(function (response) {
+                angular.copy(response.data, o.currentUser);
 
                 if (successCB)
                     successCB(response);
@@ -35,48 +35,48 @@ angular.module('UserService', [])
         /**
          * Metodo per aggiornare i dati dell'utente correntemente loggato.
          */
-        service.update = function(user, successCB, errorCB) {
+        o.update = function (user, successCB, errorCB) {
             console.info("user to update:", user);
             return $http.put(
                 server_prefix + '/user/' + user.id,
                 user)
-                .then(function(response) {
+                .then(function (response) {
                     // si può aggiornare soltanto l'utente loggato quindi aggiorno i dati....
-                    angular.copy(response.data, service.currentUser);
+                    angular.copy(response.data, o.currentUser);
 
                     if (successCB)
                         successCB(response);
-            }, errorCB);
+                }, errorCB);
         };
 
         /**
          * Metodo per eseguire una ricerca per nome/cognome
          * di persona.
          */
-        service.search = function(query, skip, reset, successCB, errorCB) {
+        o.search = function (query, skip, reset, successCB, errorCB) {
             return $http.get(server_prefix + '/user', {
                 params: {
                     where: {
                         "or": [
-                            { 
-                                "name": { "contains": query } 
+                            {
+                                "name": { "contains": query }
                             },
                             {
-                                "surname": { "contains": query } 
+                                "surname": { "contains": query }
                             }
                         ]
                     }
                 }
-            }).then(function(response) {
+            }).then(function (response) {
                 if (skip) {
                     for (var i = 0; i < response.data.length; i++) {
-                        service.users.push(response.data[i]);
+                        o.users.push(response.data[i]);
                     }
                 } else {
                     if (reset)
-                        angular.copy(response.data, service.users);
+                        angular.copy(response.data, o.users);
                     else
-                        angular.extend(service.users, response.data);
+                        angular.extend(o.users, response.data);
                 }
                 if (successCB)
                     successCB(response);
@@ -87,20 +87,20 @@ angular.module('UserService', [])
          * Metodo per richiedere la lista di utenti seguiti da un particolare
          * utente.
          */
-        service.getFollowingUsers = function(userId, order_by, skip, successCB, errorCB) {
+        o.getFollowingUsers = function (userId, order_by, skip, successCB, errorCB) {
             return $http.get(server_prefix + '/user/' + userId + '/following',
                 {
                     params: {
                         'skip': skip,
                         'order': order_by
                     }
-                }).then(function(response) {
+                }).then(function (response) {
                     if (skip) {
                         for (var i = 0; i < response.data.length; i++) {
-                            service.following_users.push(response.data[i]);
+                            o.following_users.push(response.data[i]);
                         }
                     } else {
-                        angular.extend(service.following_users, response.data);
+                        angular.extend(o.following_users, response.data);
                     }
                     if (successCB)
                         successCB(response);
@@ -112,20 +112,20 @@ angular.module('UserService', [])
          * Metodo per richiedere la lista di utenti che seguono un particolare
          * utente.
          */
-        service.getFollowerUsers = function(userId, order_by, skip, successCB, errorCB) {
+        o.getFollowerUsers = function (userId, order_by, skip, successCB, errorCB) {
             return $http.get(server_prefix + '/user/' + userId + '/follower',
                 {
                     params: {
                         'skip': skip,
                         'order': order_by
                     }
-                }).then(function(response) {
+                }).then(function (response) {
                     if (skip) {
                         for (var i = 0; i < response.data.length; i++) {
-                            service.follower_users.push(response.data[i]);
+                            o.follower_users.push(response.data[i]);
                         }
                     } else {
-                        angular.extend(service.follower_users, response.data);
+                        angular.extend(o.follower_users, response.data);
                     }
                     if (successCB)
                         successCB(response);
@@ -138,7 +138,7 @@ angular.module('UserService', [])
          * loggato. E' necessaria in quanto l'utente può avere una o più
          * immagini a secondo del metodo di iscrizione usato.
          */
-        service.getUserProfileImage = function(user) {
+        o.getUserProfileImage = function (user) {
             if (user != null) {
                 if (user.facebookImageUrl != null)
                     return user.facebookImageUrl;
@@ -153,11 +153,11 @@ angular.module('UserService', [])
         /**
          * Metodo per richiedere l'utente tramite id.
          */
-        service.getUserById = function(userId) {
+        o.getUserById = function (userId) {
             return $http.get(
                 server_prefix + '/user/' + userId
-            ).then(function(response) {
-                angular.copy(response.data, service.user);
+            ).then(function (response) {
+                angular.copy(response.data, o.user);
             });
         };
 
@@ -166,14 +166,33 @@ angular.module('UserService', [])
          * Ovviamente l'operazione viene eseguita a nome dell'utente
          * correntemente loggato.
          */
-        service.followUser = function(userToFollow, successCallback) {
-            return $http.put(server_prefix + '/user/' + userToFollow.id + '/follow').then(function(response) {
-                    service.currentUser.following += 1;
-                    userToFollow.followers += 1;
+        o.followUser = function (userToFollow, successCallback) {
+            return $http.put(server_prefix + '/user/' + userToFollow.id + '/follow').then(function (response) {
+                o.currentUser.followingCount += 1;
 
-                    userToFollow.isFollowed = true;
-                    successCallback();
-                });
+                /**
+                 *  Se il profilo visualizzato non è quello della persona loggata
+                 *  aggiorno il numero delle persone che la seguono.
+                 *  Altrimenti aggiorno il numero delle persone seguite.
+                 */
+                if (o.user.id != o.currentUser.id)
+                    o.user.followersCount += 1;//
+                else
+                    o.user.followingCount += 1;
+
+                /**
+                 *  Se il profilo visualizzato non è quello della persona loggata
+                 *  aggiungo l'utente loggato come follower.
+                 */
+                if (o.user.id != o.currentUser.id)
+                    o.follower_users.push(o.currentUser);
+                else 
+                    // aggiungo l'utente
+                    o.following_users.push(userToFollow);
+
+                userToFollow.isFollowed = true;
+                successCallback();
+            });
         };
 
         /**
@@ -181,11 +200,40 @@ angular.module('UserService', [])
          * Ovviamente l'operazione viene eseguita a nome dell'utente
          * correntemente loggato.
          */
-        service.unfollowUser = function(userToUnfollow, successCallback) {
+        o.unfollowUser = function (userToUnfollow, successCallback) {
             return $http.delete(server_prefix + '/user/' + userToUnfollow.id + '/follow')
-                .then(function(response) {
-                    service.currentUser.following -= 1;
-                    userToUnfollow.followers -= 1;
+                .then(function (response) {
+                    o.currentUser.followingCount -= 1;
+
+                    /**
+                     *  Se il profilo visualizzato non è quello della persona loggata
+                     *  aggiorno il numero delle persone che la seguono.
+                     *  Altrimenti aggiorno il numero delle persone seguite.
+                     */
+                    if (o.user.id != o.currentUser.id)
+                        o.user.followersCount -= 1;
+                    else
+                        o.user.followingCount -= 1;
+                    
+                    /**
+                     *  Se il profilo visualizzato non è quello della persona loggata
+                     *  tolgo l'utente loggato come follower.
+                     */
+                    if (o.user.id != o.currentUser.id)
+                        for (var i in o.follower_users) {
+                            if (o.follower_users[i].id == o.currentUser.id) {
+                                o.follower_users.splice(i, 1);
+                                break;
+                            }
+                        }
+                    else
+                        // rimuovo l'utente
+                        for (var i in o.following_users) {
+                            if (o.following_users[i].id == userToUnfollow.id) {
+                                o.following_users.splice(i, 1);
+                                break;
+                            }
+                        }
 
                     userToUnfollow.isFollowed = false;
                     if (successCallback)
@@ -197,31 +245,31 @@ angular.module('UserService', [])
          * Metodo per verificare se l'utente loggato 
          * sta seguendo un'altro utente.
          */
-        service.areYouFollowing = function(userToCheck, successCallback) {
+        o.areYouFollowing = function (userToCheck, successCallback) {
             return $http.get(server_prefix + '/user/following/' + userToCheck.id)
-                .then(function(response) {
+                .then(function (response) {
 
                     userToCheck.isFollowed = true;
                     if (successCallback)
                         successCallback(response);
 
-                }, function(response) {
+                }, function (response) {
                     userToCheck.isFollowed = false;
                 });
         };
 
-        service.toggleFollow = function(user) {
+        o.toggleFollow = function (user) {
             if (user.isFollowed == true) {
-                service.unfollowUser(user, function() {
+                o.unfollowUser(user, function () {
                     //fatto
                 })
             } else {
-                service.followUser(user, function() {
+                o.followUser(user, function () {
                     //fatto
                 })
             }
         };
 
 
-        return service;
+        return o;
     }]);
