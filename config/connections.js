3484fed9742085847726ef055cb24b1f10beacad
Mariusz Wiazowski <mariusz.wiazowski@gmail.com>
2018-02-08 18:17:45 +0100

    Changed default db name

--- config/connections.js
+++ config/connections.js
@@ -64,7 +64,7 @@ module.exports.connections = {
     port: 27017,
     // user: 'username',
     // password: 'password',
-    database: 'news'
+    database: 'tasty_db'
   },
 
   /***************************************************************************
